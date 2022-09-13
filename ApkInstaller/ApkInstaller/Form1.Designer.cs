0575c0bac5d64a074edce7677522e7d5c8bf968d
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2022-09-13 16:34:12 -0700

    Add files via upload

---
+++ ApkInstaller/ApkInstaller/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace ApkInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            this.APKLabel = new System.Windows.Forms.Label();
+            this.APK_Path = new System.Windows.Forms.TextBox();
+            this.SelectButton = new System.Windows.Forms.Button();
+            this.InstallButton = new System.Windows.Forms.Button();
+            this.TestButton = new System.Windows.Forms.Button();
+            this.SetupButton = new System.Windows.Forms.Button();
+            this.MainHelpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // APKLabel
+            // 
+            this.APKLabel.AutoSize = true;
+            this.APKLabel.Location = new System.Drawing.Point(12, 9);
+            this.APKLabel.Name = "APKLabel";
+            this.APKLabel.Size = new System.Drawing.Size(32, 15);
+            this.APKLabel.TabIndex = 0;
+            this.APKLabel.Text = "APK:";
+            // 
+            // APK_Path
+            // 
+            this.APK_Path.Location = new System.Drawing.Point(50, 6);
+            this.APK_Path.Name = "APK_Path";
+            this.APK_Path.ReadOnly = true;
+            this.APK_Path.Size = new System.Drawing.Size(277, 23);
+            this.APK_Path.TabIndex = 1;
+            // 
+            // SelectButton
+            // 
+            this.SelectButton.Location = new System.Drawing.Point(333, 6);
+            this.SelectButton.Name = "SelectButton";
+            this.SelectButton.Size = new System.Drawing.Size(110, 22);
+            this.SelectButton.TabIndex = 2;
+            this.SelectButton.Text = "Select APK...";
+            this.SelectButton.UseVisualStyleBackColor = true;
+            this.SelectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.Location = new System.Drawing.Point(449, 6);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(75, 23);
+            this.InstallButton.TabIndex = 3;
+            this.InstallButton.Text = "Install";
+            this.InstallButton.UseVisualStyleBackColor = true;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
+            // 
+            // TestButton
+            // 
+            this.TestButton.Location = new System.Drawing.Point(530, 6);
+            this.TestButton.Name = "TestButton";
+            this.TestButton.Size = new System.Drawing.Size(75, 23);
+            this.TestButton.TabIndex = 4;
+            this.TestButton.Text = "Test ADB";
+            this.TestButton.UseVisualStyleBackColor = true;
+            this.TestButton.Click += new System.EventHandler(this.TestButton_Click);
+            // 
+            // SetupButton
+            // 
+            this.SetupButton.Location = new System.Drawing.Point(611, 5);
+            this.SetupButton.Name = "SetupButton";
+            this.SetupButton.Size = new System.Drawing.Size(75, 23);
+            this.SetupButton.TabIndex = 5;
+            this.SetupButton.Text = "Setup";
+            this.SetupButton.UseVisualStyleBackColor = true;
+            this.SetupButton.Click += new System.EventHandler(this.SetupButton_Click);
+            // 
+            // MainHelpButton
+            // 
+            this.MainHelpButton.Location = new System.Drawing.Point(692, 5);
+            this.MainHelpButton.Name = "MainHelpButton";
+            this.MainHelpButton.Size = new System.Drawing.Size(75, 23);
+            this.MainHelpButton.TabIndex = 6;
+            this.MainHelpButton.Text = "Help";
+            this.MainHelpButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 38);
+            this.Controls.Add(this.MainHelpButton);
+            this.Controls.Add(this.SetupButton);
+            this.Controls.Add(this.TestButton);
+            this.Controls.Add(this.InstallButton);
+            this.Controls.Add(this.SelectButton);
+            this.Controls.Add(this.APK_Path);
+            this.Controls.Add(this.APKLabel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "APK Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label APKLabel;
+        private System.Windows.Forms.TextBox APK_Path;
+        private System.Windows.Forms.Button SelectButton;
+        private System.Windows.Forms.Button InstallButton;
+        private System.Windows.Forms.Button TestButton;
+        private System.Windows.Forms.Button SetupButton;
+        private System.Windows.Forms.Button MainHelpButton;
+    }
+}
+
---
+++ ApkInstaller/ApkInstaller/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApkInstaller {
+
+    public partial class Form1 : Form {
+        string File = string.Empty;
+        string SubsystemAddress = string.Empty;
+        bool IsTested = false;
+        bool IsSetup = false;
+        bool IsApkSelected = false;
+
+        public Form1() {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;   
+
+        }
+        void ChangeName( string NewName ) {
+            this.Text = NewName;
+        }
+
+        private void SelectButton_Click( object sender, EventArgs e ) {
+            using ( OpenFileDialog ApkSelector = new System.Windows.Forms.OpenFileDialog() ) {
+                ApkSelector.InitialDirectory = System.Environment.GetFolderPath( System.Environment.SpecialFolder.UserProfile );
+                ApkSelector.Filter = "APK Files (*.apk)|*.apk|All Files (*.*)|*.*";
+
+                if( ApkSelector.ShowDialog() == DialogResult.OK ) {
+                    APK_Path.Text = ApkSelector.SafeFileName;
+                    File = ApkSelector.FileName;
+                    IsApkSelected = true;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            /*
+            if(
+                MessageBox.Show(
+                    "Please note: usage of this tool requires the Windows Subsystem for Android to be installed and for the Android SDK Platform Tools to be in the system path variable. You can test if the tools work with the Test ADB button. Would you like instructions for this?",
+                    "APK Installer",
+                    MessageBoxButtons.YesNo
+                ) == DialogResult.Yes
+            ) {
+                MessageBox.Show("yes");
+            }
+            */
+        }
+
+        private void TestButton_Click( object sender, EventArgs e ) {
+            System.Diagnostics.Process TestADB = new System.Diagnostics.Process();
+
+            TestADB.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            TestADB.StartInfo.FileName = "cmd.exe";
+            TestADB.StartInfo.Arguments = "/c adb";
+            TestADB.StartInfo.UseShellExecute = false;
+            TestADB.StartInfo.RedirectStandardOutput = true;
+
+            TestADB.Start();
+            string ADBOutput = TestADB.StandardOutput.ReadToEnd();
+            TestADB.WaitForExit();
+
+            //MessageBox.Show( ADBOutput );
+
+            if( ADBOutput.Contains( "Android Debug Bridge version" ) || ADBOutput.Contains( "global options:" ) ) {
+                MessageBox.Show( "Android SDK Tools are installed. The tool will work as expected.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information );
+                IsTested = true;
+            } else
+            {
+                IsTested = false;
+                MessageBox.Show( "Android SDK Tools are not installed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+            }
+        }
+
+        public void SetupForm() {
+            System.Windows.Forms.Form Setup = new System.Windows.Forms.Form();
+
+            Setup.MinimizeBox = false;
+            Setup.MaximizeBox = false;
+            Setup.FormBorderStyle = FormBorderStyle.FixedDialog;
+            Setup.Size = new Size( 300, 100 );
+            Setup.StartPosition = FormStartPosition.CenterScreen;
+            Setup.Text = "Setup";
+
+            WebBrowser WB1 = new WebBrowser();
+
+            string HelpHTML =
+@"
+<html>
+<style>
+body {
+    background: white;
+    font-family: 'Verdana';
+}
+</style>
+
+<body>
+<h1>Setup Help Page</h1>
+
+<p>
+Setup will connect the ADB (Android Debug Bridge) to the Android Subsystem. Without this connection, apps cannot be installed.
+</p>
+
+<p>
+Before beginning this guide, ensure that the Windows Subsystem for Android is installed. You can verify if it is installed by searching for its settings in Start.
+</p>
+
+</body>
+
+</html>
+";
+
+            WB1.DocumentText = HelpHTML;
+            WB1.Size = new Size( 600, 600 );
+            WB1.Location = new Point( 0, 0 );
+
+            Label InfoLabel = new Label();
+            InfoLabel.Text = "Subsystem Developer IP:";
+            InfoLabel.Location = new Point( 10, 10 );
+            InfoLabel.Size = new Size( 140, 15 );
+
+            TextBox SubsystemIP = new TextBox();
+            SubsystemIP.Size = new Size( 125, 23 );
+            SubsystemIP.Location = new Point( InfoLabel.Right + 5, 5 );
+            
+            Button FinishButton = new Button();
+            FinishButton.Text = "Finish";
+            FinishButton.Location = new Point( Setup.Right - 100, Setup.Bottom - 70 );
+            
+            Button HelpButton = new Button();
+            HelpButton.Text = "Help";
+            HelpButton.Location = new Point( FinishButton.Left - 80, Setup.Bottom - 70 );
+
+
+
+            Setup.Controls.Add( InfoLabel );
+            Setup.Controls.Add( SubsystemIP );
+            Setup.Controls.Add( FinishButton );
+            Setup.Controls.Add( HelpButton );
+
+            
+            void Help( object Sender, EventArgs E ) {
+                Form HelpBox = new Form();
+
+                HelpBox.Size = new Size( 600, 600 );
+                HelpBox.MinimizeBox = false;
+                HelpBox.MaximizeBox = false;
+                HelpBox.FormBorderStyle = FormBorderStyle.FixedDialog;
+                HelpBox.Text = "Help";
+                
+                HelpBox.Controls.Add( WB1 );
+                
+                HelpBox.ShowDialog();
+            }
+
+            void Finish( object Sender, EventArgs E ) {
+                SubsystemAddress = SubsystemIP.Text;
+                
+                if( IsTested && SubsystemAddress != "" ) {
+                    ChangeName( "APK Installer - " + SubsystemAddress );
+                    IsSetup = true;
+                } else if( SubsystemAddress == "" && IsTested ) {
+                    MessageBox.Show( "You must provide an IP address to connect to the Subsystem. See Help for more info and instructions.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                } else if( !IsTested ) {
+                    MessageBox.Show( "You must test the adb tools before attempting to connect to a Subsystem.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                }
+
+                Setup.Dispose();
+            }
+
+            HelpButton.Click += Help;
+            FinishButton.Click += Finish;
+
+            Setup.ShowDialog();
+        }
+
+        private void SetupButton_Click( object sender, EventArgs e ) {
+            SetupForm();
+        }
+
+        private void InstallButton_Click( object sender, EventArgs e ) {
+            if( IsSetup && IsApkSelected ) {
+                MessageBox.Show( "Install" );
+            } else if( !IsSetup ) {
+                MessageBox.Show( "You must complete setup before installing.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else if( !IsApkSelected ) {
+                MessageBox.Show( "No APK selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
